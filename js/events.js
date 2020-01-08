142b48c783b74235f97b53921606f06e1ff70504
daring-array-6961 <ebrahimnery96@gmail.com>
2020-01-08 02:55:37 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -55,7 +55,7 @@ function submitIt() {
 
   $("form").on("submit", function() {
   if ($( "input:first" ).val() === "correct") {
-    alert('your form is going to be submitted now');
+    alert("Your form is going to be submitted now.");
     return;
   }
     alert("you entered the wrong value");
