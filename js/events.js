effd4927870bf5c3ee315e01f20b95ea921f0212
daring-array-6961 <ebrahimnery96@gmail.com>
2020-01-08 03:46:36 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -54,10 +54,9 @@ function submitIt() {
 
 
   $("form").on("submit", function() {
-  //if ($( "input:first" ).val() === "correct") {
+
     alert("Your form is going to be submitted now.");
-    //return;
-  //}
+
 
   });
 }
